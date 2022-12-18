e000780528471ce3c134c62bdcd676387b344ba5
Christopher Valerio <christopher@valerio.guru>
2022-12-18 16:57:13 -0600

    adding optparse

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
+import qualified Data.ByteString.Char8 as BC
+import Options.Applicative
+
+
+
+
+--citadel_project :: BC.ByteString
+--citadel_project = "23124553"
+
+gitlab_url :: BC.ByteString -> BC.ByteString
+gitlab_url gitlab_project = "https://gitlab.com/api/v4/projects/" <> gitlab_project <> "/pipelines"
+
+
+data Args = Args
+    { project :: String
+    , pToken :: String
+    }
+
+argParser :: Parser Args
+argParser = Args
+    <$> strOption
+        (long "project" <> metavar "PROJECT_ID" <> help "Gitlab Project ID")
+    <*> strOption
+        (long "pToken" <> metavar "PERSONAL_TOKEN" <> help "Personal gitlab token" )
+
+
+printArgs :: Args -> IO ()
+printArgs (Args p t) = putStrLn $ p ++ " " ++ t
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = execParser opts >>= printArgs
+  where
+    opts = info (argParser <**> helper )
+      ( fullDesc 
+      <> progDesc "Get gitlab stuff"
+      <> header "gitlab-pipes a tool for listing gitlab pipelines")
+
+
+
